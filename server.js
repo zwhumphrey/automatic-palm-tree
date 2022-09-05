6ab734a034c728678eb7c876d9ee9528ba9e03a6
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-05 11:47:41 -0500

    added package.json readme .gitignore and folders for routes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/automatic-palm-tree',
+  process.env.MONGODB_URI || 'mongodb://localhost/automatic-palm-tree',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
