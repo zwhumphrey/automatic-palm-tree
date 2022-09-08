895d758be3224942ae58c58b7acdad1f899aba44
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-08 18:56:41 -0500

    added array of id values ref the thought/user models for friends and thoughts

--- models/user-model.js
+++ models/user-model.js
@@ -1,16 +1,33 @@
 const { Schema, model } = 'mongoose';
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    require: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      require: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      match: [/.+\@.+\..+/],
+    },
+    thoughts: [{ type: Schema.typers.ObjectId, ref: 'Thought-model' }],
+    friends: [{ type: Schema.TypesObjectId, ref: 'User-model' }],
   },
-  email: {
-    type: String,
-    require: true,
-    unique: true,
-    match: [/.+\@.+\..+/],
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+virtual('friendCount').get(function () {
+  return this.friends.length;
 });
+
+const User = model('User', userSchema);
+
+model.exports = User;
