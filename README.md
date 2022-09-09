b46a55f44fc319c7f88348992b17c926074d02ac
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-08 21:54:08 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Social Media app
+
+## Description
+
+A social media app that is built with Express, Mongoose, and MongoDB.
+
+## User Story
+
+AS A social media startup
+I WANT an API for my social network that uses a NoSQL database
+SO THAT my website can handle large amounts of unstructured data
+
+## Usage
+
+Install MongoDB then clone the repo. Make sure you install dependencies and do npm start. You can use your browser to test it.
+
+## Questions
+
+If you have comments or questions please let me know at [GitHub](https://www.github.com/zwhumphrey)
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,4 +10,23 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate([
+        { path: 'thoughts', select: '-__v' },
+        { path: 'friends', select: '-__v' },
+      ])
+      .select('-__v')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 };
