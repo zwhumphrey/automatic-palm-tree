45e2f4c0515b11c8bec818bd1391e9e47dd3bcbd
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-08 20:10:36 -0500

    started the thought route need to add link to controller once finished

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,1 +1,22 @@
-const { Router } = require('express');
+const router = require('express').Router();
+const {
+  getAllThoughts,
+  GetThoughtsById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require('../../controllers'); // add route to controller
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router
+  .route('/:id')
+  .get(getThoughtsById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/:thoughtId/reactions/').post(addReaction).delete(deleteReaction);
+
+module.exports = router;
